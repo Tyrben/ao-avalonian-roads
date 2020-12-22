dbf944ca156019c64ac6d9e9b5737cb703502224
Germain Barret <sandburg@netcourrier.com>
2020-12-22 18:15:47 +0100

    Switching a shell script in Unix new line format.

--- scripts/convert_world_xml_to_csv.sh
+++ scripts/convert_world_xml_to_csv.sh
@@ -3,6 +3,9 @@
 # Prerequisites: install xidel
 #http://videlibri.sourceforge.net/xidel.html#downloads
 
+# Data commun from https://github.com/broderickhyman/ao-bin-dumps
+# direct link: https://github.com/broderickhyman/ao-bin-dumps/raw/master/cluster/world.xml
+
 xidel world.xml -e 'concat(/world/clusters/cluster[@id="1206"]/@id , ";", /world/clusters/cluster[@id="1206"]/@displayname)'
 
 xidel /mnt/d/git/autres/ao-bin-dumps/cluster/world.xml -e 'concat(/world/clusters/cluster[number(@id)<9999]/@displayname, ";", /world/clusters/cluster[number(@id)<9999]/@id)'
@@ -14,4 +17,4 @@ xidel /mnt/d/git/autres/ao-bin-dumps/cluster/world.xml -e '/world/clusters/clust
 xidel /mnt/d/git/autres/ao-bin-dumps/cluster/world.xml -e '/world/clusters/cluster[ fn:matches(@id, "^[0-9]{4}$") ]/fn:concat(@id, ";", @displayname, ";", @type)'
 
 # Info regarding exists (portals)
-xidel /mnt/d/git/autres/ao-bin-dumps/cluster/world.xml -e '/world/clusters/cluster[ fn:matches(@id, "^[0-9]{4}$") ]/exits/exit[fn:matches(@targettype, "^Cluster$")]/fn:concat(../../@id, ";", fn:substring-after(@targetid, "@"))'
+xidel https://github.com/broderickhyman/ao-bin-dumps/raw/master/cluster/world.xml -e '/world/clusters/cluster[ fn:matches(@id, "^[0-9]{4}$") ]/exits/exit[fn:matches(@targettype, "^Cluster$")]/fn:concat(../../@id, ";", fn:substring-after(@targetid, "@"))'
