62bf8567938b5e776715d48a84524e3250edb3cf
Germain Barret <sandburg@netcourrier.com>
2021-05-26 12:35:48 +0200

    Relocating lib and command line application sources into src. Retake xidel commands in silence mode which avoid the header info and the script workaround.

--- scripts/convert_world_xml_to_csv.sh
+++ scripts/convert_world_xml_to_csv.sh
@@ -8,9 +8,9 @@
 # direct link: https://github.com/broderickhyman/ao-bin-dumps/raw/master/cluster/world.xml
 
 # Info regarding cluster zones (maps)
-{ echo "Id,Name,Type" ; xidel https://github.com/broderickhyman/ao-bin-dumps/raw/master/cluster/world.xml -e '/world/clusters/cluster[ fn:matches(@id, "^(PSG-)?[0-9]{4}$") ]/fn:concat(@id, ",", @displayname, ",", @type)' 2> /dev/null | sed 's/PSG-/toDelete/2' | sed -E 's/[0-9]{4}/toDelete/2' | sed '/toDelete/d'; } > static-maps.csv
+{ echo "Id,Name,Type" ; xidel --silent https://github.com/broderickhyman/ao-bin-dumps/raw/master/cluster/world.xml -e '/world/clusters/cluster[ fn:matches(@id, "^(PSG-)?[0-9]{4}$") ]/fn:concat(@id, ",", @displayname, ",", @type)' | sed 's/PSG-/toDelete/2' | sed -E 's/[0-9]{4}/toDelete/2' | sed '/toDelete/d'; } > static-maps.csv
 
 # Info regarding exits (portals)
-{ echo "Origin,Destination" ; xidel https://github.com/broderickhyman/ao-bin-dumps/raw/master/cluster/world.xml -e '/world/clusters/cluster[ fn:matches(@id, "^(PSG-)?[0-9]{4}$") ]/exits/exit[ fn:matches(@targettype, "^Cluster$") ]/fn:concat(../../@id, ",", fn:substring-after(@targetid, "@"))' 2> /dev/null | sed -n -E '/^[0-9]{4};[0-9]{4}$/p'; } > static-portals.csv
+{ echo "Origin,Destination" ; xidel --silent https://github.com/broderickhyman/ao-bin-dumps/raw/master/cluster/world.xml -e '/world/clusters/cluster[ fn:matches(@id, "^(PSG-)?[0-9]{4}$") ]/exits/exit[ fn:matches(@targettype, "^Cluster$") ]/fn:concat(../../@id, ",", fn:substring-after(@targetid, "@"))' | sed -n -E '/^[0-9]{4},[0-9]{4}$/p'; } > static-portals.csv
 
 # Alternate URL: https://raw.githubusercontent.com/broderickhyman/ao-bin-dumps/master/cluster/world.xml
